f874d330342c15b0187690825746a3dfbd18bcbc
bd77 <bartdegraeuwe@hotmail.com>
2019-05-17 16:31:51 +0200

    Add script that cuts out the domain from the OTM data per NUTS3 region

---
+++ create_network_shp_utm.R
@@ -0,0 +1,61 @@
+# --------------------------------------------
+# create network shape file of a domain in UTM
+# --------------------------------------------
+
+library(rgdal)
+library(raster)
+library(rgeos)
+source("long2UTM.R")
+
+create.network.shp.utm <- function(city.extent, cityname, nuts3.bbox.df, otm.path) {
+  # list with boundary boxes of nuts3 areas
+  nuts3.in.city.domain <- c()
+  for (i in 1:NROW(nuts3.bbox.df)) {
+    extent.nuts3 <- extent(c(nuts3.bbox.df$lon_min[i], nuts3.bbox.df$lon_max[i],
+                             nuts3.bbox.df$lat_min[i], nuts3.bbox.df$lat_max[i]))
+    if (!is.null(intersect(extent.nuts3, city.extent))) {
+      nuts3 <- toString(nuts3.bbox.df$nuts3[i])
+      nuts3.in.city.domain <- c(nuts3.in.city.domain, nuts3)
+    }
+  }
+  shp.union <- NULL
+  for (nuts3 in nuts3.in.city.domain) {
+    nuts3.shp.folder <- paste0(otm.path, nuts3)
+    nuts3.shp.name <- paste0("traffic_roadlinks_", nuts3)
+    shp.orig <- readOGR(nuts3.shp.folder, nuts3.shp.name)
+    shp.cropped <- crop(shp.orig, city.extent)
+    if (is.null(shp.union)) {
+      shp.union <- shp.cropped
+    } else if (!is.null(shp.cropped)) {
+      shp.union <- bind(shp.union, shp.cropped)
+    }
+  }
+  # convert to UTM
+  utm.string <- long2UTM(mean(city.extent@xmin, city.extent@xmax))
+  shp.utm <- spTransform(shp.union, CRS(utm.string))
+  
+  # plot the domain
+  png(file.path(city.output.folder, paste0(cityname, "_utm.png")), width = 4*480, height = 4*480,
+      pointsize = 4*12)
+  plot(extent(shp.utm), col="black", xlab = "x (m)", ylab = "y (m)", main = cityname, asp = 1)
+  functional.list <- c('mainRoad', 'firstClass', 'secondClass', 'thirdClass', 'fourthClass')
+  functional.color <- c("red", "blue", "light blue", "green", "light green")
+  functional.lwd <- c(5, 4, 3, 2, 1)
+  for (i.f in 1:5) {
+    functional <- functional.list[i.f]
+    plot(shp.utm[shp.utm@data$functional == functional,], col = functional.color[i.f], 
+         lwd = functional.lwd[i.f], add = TRUE)
+  }
+  dev.off()
+  
+  # return the result  
+  return(shp.utm)
+  
+}
+
+# i <- 1
+# otm.path <- "D:/SHERPAcity/OpenTransportMap/OTM_NUTS3_corrected_20181002/OTM_NUTS3_corrected_20181002/"
+# city.extent <- extent(c(4.3106, 4.4122, 50.8126, 50.8782))
+# intersect(extent.nuts3, city.extent)
+# intersect
+# nuts3 <- "BE100"
